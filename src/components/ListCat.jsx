1f723f0b91bcadbf088d89b9f72c13391292fa7d
anglczbla <angelicaizabella90@gmail.com>
2025-12-07 22:54:56 +0700

    add pagination

--- src/components/ListCat.jsx
+++ src/components/ListCat.jsx
@@ -26,9 +26,36 @@ const ListCat = () => {
     <div>
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error loading cats</p>}
+
       {cat?.data?.map((item) => (
-        <div key={item.id}>{item.name}</div>
+        <div key={item.id}>
+          <img src={item.image} alt={item.name} width={300} />
+          <p>
+            <strong>Name:</strong> {item.name}
+          </p>
+          <p>
+            <strong>Origin:</strong> {item.origin}
+          </p>
+        </div>
       ))}
+
+      <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
+        {[...Array(cat?.totalPages || 0)].map((_, index) => (
+          <button
+            key={index}
+            style={{
+              color: page === index + 1 ? "red" : "white",
+              backgroundColor: page === index + 1 ? "white" : "black",
+              padding: "10px 15px",
+              cursor: "pointer",
+              border: "1px solid white",
+            }}
+            onClick={() => setPage(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
